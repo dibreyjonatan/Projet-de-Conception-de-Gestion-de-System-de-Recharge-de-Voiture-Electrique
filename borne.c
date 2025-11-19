b62ab4b399154878080cf3481ad8b7bdbd7dd780
dibreyjonatan <kamdadibreyjonatan@gmail.com>
2025-11-19 09:04:33 +0100

    Feature : Ajout d'authentification

--- borne.c
+++ borne.c
@@ -2,6 +2,7 @@
 #include <donnees_borne.h>
 
 #include "lecteurcarte.h"
+#include "baseclient.h"
 
 int main()
 {
@@ -13,6 +14,8 @@ int main()
         lecteurcarte_initialiser();
         int numero=lecteurcarte_lire_carte();
         printf("numero lu est : %d\n",numero); 
+        int found=baseclient_authentifier(numero);
+        printf("resultat authentification : %d\n",found) ;
     }
 
 }

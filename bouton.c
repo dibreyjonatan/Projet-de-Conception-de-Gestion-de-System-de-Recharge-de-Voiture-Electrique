cd25d1069ace666c0850b7ef9195099ab4fb6a07
dibreyjonatan <kamdadibreyjonatan@gmail.com>
2025-11-19 22:48:53 +0100

    Feature: Implementation de la fonction de lecture bouton

---
+++ bouton.c
@@ -0,0 +1,12 @@
+#include "bouton.h"
+
+entrees *io_b ;
+int shmid_b ;
+
+int button_appuie_button_charge(){
+    io_b= acces_memoire(&shmid_b) ;
+    return ( io_b -> bouton_charge ) ;
+}
+
+int button_apppuie_button_stop() ;
+
---
+++ bouton.h
@@ -0,0 +1,15 @@
+#ifndef BUTTON_H
+#define BUTTON_H
+
+#include <stdio.h>
+#include <donnees_borne.h>
+#include <memoire_borne.h>
+#include <unistd.h>
+
+
+int button_appuie_button_charge();
+int button_apppuie_button_stop() ;
+
+
+
+#endif 

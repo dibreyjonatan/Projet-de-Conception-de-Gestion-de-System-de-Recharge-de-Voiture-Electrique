90af6ef772441da3b8e5681473b4944e3f21a93d
dibreyjonatan <kamdadibreyjonatan@gmail.com>
2026-03-21 23:19:14 +0100

    [DELETE]: Suppression des fichiers .c et .h pour demarrer le developpement en cpp

--- baseclient.c
+++
@@ -1,6 +0,0 @@
-#include <unistd.h>
-#include "baseclient.h"
-
-int baseclient_authentifier(int numcarte){
-    return 0;
-}
--- baseclient.h
+++
@@ -1,9 +0,0 @@
-#ifndef BASECLIENT_H
-#define BASECLIENT_H
-#include <stdlib.h>
-#include <stdio.h>
-
-// A ajouter (attribut + méthodes)
-int  baseclient_authentifier(int numcarte);
-
-#endif 

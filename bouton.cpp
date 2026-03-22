fef9ad8aac3cfeef2537941778962238cafd6163
dibreyjonatan <kamdadibreyjonatan@gmail.com>
2026-03-22 01:33:44 +0100

    [FIX] correction

--- bouton.cpp
+++ bouton.cpp
@@ -3,17 +3,17 @@ Bouton::Bouton(){
     int shmid_b ;
     io_b= acces_memoire(&shmid_b) ;
 }
-int bouton_appuie_button_charge(){
+int Bouton::bouton_appuie_button_charge(){
   return ( io_b -> bouton_charge ) ;
 }
-int bouton_apppuie_button_stop() {
+int Bouton::bouton_apppuie_button_stop() {
  return ( io_b -> bouton_stop ) ;
 }
 
-void bouton_set_bouton_charge(){
+void Bouton::bouton_set_bouton_charge(){
   io_b -> bouton_charge=0 ;
 }
 
-void bouton_set_bouton_stop(){
+void Bouton::bouton_set_bouton_stop(){
  io_b -> bouton_stop=0 ;
 }
